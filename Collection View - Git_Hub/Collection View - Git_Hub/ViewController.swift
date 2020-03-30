0d4d0ae21375a411134b67b0392f247e739f4b1a
Yasin <yasinmemon482@yahoo.com>
2020-03-30 22:45:37 +0500

    All Reset

--- Collection View - Git_Hub/Collection View - Git_Hub/ViewController.swift
+++ Collection View - Git_Hub/Collection View - Git_Hub/ViewController.swift
@@ -12,61 +12,14 @@ import Alamofire
 class ViewController: UIViewController {
 
     // MARK :- Variables
-    var arrayOFImages : [UIImage] = [UIImage(imageLiteralResourceName: <#T##String#>)]
+    var imageArray = ["image1","image2","image3","image4","image5","image6","image7"]
     //MARK :- Outlet
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
     override func viewDidLoad() {
-        
-        
-     
-        /* Alamofire.request("https://api.unsplash.com/photos/",
-                           method: .get,
-                           parameters: ["YttK1qKKQ0j7DEuQixvyxuZJSY1iVTPRIAMXLlzznPk": ".latest"])
-         .validate()
-         .responseJSON { response in
-           guard response.result.isSuccess else {
-            // print("Error while fetching remote rooms:\(String(describing: response.result.error)" )
-            // completion(nil)
-             return
-           }
-            //print(value)
-           guard let value = response.result.value as? [UIImage: Any],
-             let rows = value["rows"] as? [[UIImage: Any]] else {
-
-               print("Malformed data received from fetchAllRooms service")
-              // completion(nil)
-               return
-           }
-
-                    }
-         
-        */
-        
         super.viewDidLoad()
-        
-        
-     /*   request(.GET, method: HTTPMethod(rawValue: "https://api.unsplash.com/photos/")!, parameters: ["YttK1qKKQ0j7DEuQixvyxuZJSY1iVTPRIAMXLlzznPk": ".latest"])
-        .responseSwiftyJSON { (request, response, json, error) in
-                    println(json)
-                    println(error)
-                  }
-        
-       */
-        
-        Alamofire.request("https://pixabay.com/api/?key=15810784-31acc612296e0df4b99e752e9&q=yellow+flowers&image_type=photo&pretty=true",method: .get, parameters: ["photo":"latest"]).responseString{(respons) in
-            if respons.result.isSuccess{
-                print("Success With Samur")
-                print(respons.result.value!)
-                if let data = respons.data{
-                    self.parseData(JSONData: data)
-                    //self.arrayOFImages.append(UIImage(data: data)!)
-                }
-            }else{
-                print("Failed")
-            }
-        }
+      
         
         print("App Launched")
         if let layout = collectionView.collectionViewLayout as? PinterestLayout{
@@ -74,7 +27,6 @@ class ViewController: UIViewController {
         }
         collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
-
     func parseData(JSONData : Data){
         do{
         let readableJSON = try JSONSerialization.jsonObject(with: JSONData, options:.allowFragments) as! [[UIImage:AnyObject]]
@@ -89,8 +41,8 @@ class ViewController: UIViewController {
 
 extension ViewController: PinterestLayoutDelegate{
     func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
-        let image = viewModel.cellViewModels[indexPath.item].image
-        let height = image.size.height
+        let image = UIImage(named: imageArray[indexPath.item])
+        let height = image!.size.height
         return height
     }
     
@@ -99,21 +51,17 @@ extension ViewController: PinterestLayoutDelegate{
 
 extension ViewController : UICollectionViewDataSource,UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.cellViewModels.count
+        return imageArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
-        let image = viewModel.cellViewModels[indexPath.row].image
-        cell.imageView.image = image
-        //cell.imageView.frame.size.width = cell.frame.size.width
+        let image = UIImage(named: imageArray[indexPath.item])
+        cell.imageView.image = image as! UIImage
         return cell
     }
     
     
 }
-extension ViewController{
-    //ALOMAFIRE
-   
-}
+
